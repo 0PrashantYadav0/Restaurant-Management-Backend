770abf1a7d9411fba9344c3e7331770afeb4cd07
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2024-12-24 12:55:47 +0530

    feat: add controllers for food, invoice, menu, order, order item, table, and user

---
+++ controllers/userController.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func CreateUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+	}
+}
+
+func HashPassword(password string) string {
+	return password
+}
+
+func VerifyPassword(hashedPassword, password string) bool {
+	return hashedPassword == password
+}
