ccae684f8de259479ff90e309af59a41134b374b
JoyBoy <144602492+0PrashantYadav0@users.noreply.github.com>
2024-12-25 20:45:52 +0530

    fix: update validation tags in model structs for consistent formatting

--- models/menuModel.go
+++ models/menuModel.go
@@ -8,8 +8,8 @@ import (
 
 type Menu struct {
 	ID         primitive.ObjectID `bson:"_id"`
-	Name       *string            `json:"name" validate:"required, min=2, max=100"`
-	Category   *string            `json:"category" validate:"required, min=2, max=100"`
+	Name       *string            `json:"name" validate:"required,min=2,max=100"`
+	Category   *string            `json:"category" validate:"required,min=2,max=100"`
 	Start_Date *time.Time         `json:"start_date"`
 	End_Date   *time.Time         `json:"end_date"`
 	Created_at time.Time          `json:"created_at"`
